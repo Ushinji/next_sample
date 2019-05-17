0c558aa17fc5f7a8f13b11c150f6c9cd58978017
Ushinji <siguma971@gmail.com>
2019-05-18 00:24:58 +0900

    Add Styled-component setting for TypeScript

--- src/components/pages/RootPage/index.tsx
+++ src/components/pages/RootPage/index.tsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+const H1 = styled.h1``;
+const P = styled.p``;
+
 const Top = () => {
-  return <div>IndexPage</div>;
+  return (
+    <Container>
+      <H1>IndexPage</H1>
+      <P>Next.js sample application.</P>
+    </Container>
+  );
 };
 
 export default Top;
