83aa68689981865f109f89f8f9b167eed693e982
Ushinji <siguma971@gmail.com>
2019-05-19 15:32:53 +0900

    Change relative path import

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withTypescript = require("@zeit/next-typescript");
 
 module.exports = withTypescript({
   webpack(config, options) {
+    config.resolve.alias["src"] = path.join(__dirname, "src");
     config.resolve.alias["components"] = path.join(__dirname, "components");
     return config;
   }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import * as Router from "koa-router";
-import RootController from "src/controller/root_controller";
+import RootController from "../controller/root_controller";
 
 const router = new Router();
 router.get("/", RootController.getRootPage);
