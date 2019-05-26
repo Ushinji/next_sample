4ef43380f801804acb743c95ff4a4693a4dc171c
Ushinji <siguma971@gmail.com>
2019-05-26 10:30:51 +0900

    Fix `eslint-disable` in routes.ts

--- routes.ts
+++ routes.ts
@@ -1,5 +1,6 @@
-/* eslint-disable */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const nextRoutes = require('next-routes');
+
 const routes = nextRoutes()
   .add('Top', '/')
   .add('ProjectList', '/projects')
