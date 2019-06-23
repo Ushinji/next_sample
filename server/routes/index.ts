f391b86fd982611ae80146577be067c9d5833ba0
Ushinji <siguma971@gmail.com>
2019-06-23 20:02:34 +0900

    Edit routing for api server.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,11 +1,8 @@
 import * as Router from 'koa-router';
-import RootController from '../controller/root_controller';
 import ProjectController from '../controller/project_controller';
 
 const router = new Router();
-router.get('/', RootController.getRootPage);
 router.get('/api/projects/:id', ProjectController.getProject);
-router.options('/api/projects/:id');
 router.put('/api/projects/:id', ProjectController.updateProject);
 router.get('/api/projects', ProjectController.getProjects);
 
